2b6a85611ccebfcddc5481554ce5080f485ac590
Conner Catanese (RIT Student) <clc8254@ad.rit.edu>
2017-10-02 16:08:50 -0400

    practice

---
+++ web_app_0/script.js
@@ -0,0 +1,12 @@
+// const
+const answer = 42;
+// string concatenation
+console.log("The answer to everything is " + answer);
+
+// math
+let x = 1;
+let y = 1;
+let sum = x + y;
+
+// string templating (ES6 only)
+console.log(`${x} + ${y} is ${sum}`);
